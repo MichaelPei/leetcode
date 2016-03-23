43208897e70673375d1f072dc56d5e8f8b2a7b42
Michael Pei <ambitious.pei@gmail.com>
2016-03-23 17:04:36 +0800

    [Accepted]committed by LeetCode Extension

--- 202-Happy-Number/solution.swift
+++ 202-Happy-Number/solution.swift
@@ -3,10 +3,8 @@ class Solution {
         var number = n
         var newNumber = 0
         while number > 0 {
-            NSLog("Begin: number:: \(number), new: \(newNumber)")
             newNumber += Int(pow(Double(number % 10), 2))
             number /= 10
-            NSLog("End: number:: \(number), new: \(newNumber)")
         }
         
         if newNumber < 10 && newNumber != 7{
