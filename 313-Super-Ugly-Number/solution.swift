2ab13cbc5b7bf9739fff90a3ddb3d63069ce829a
Michael Pei <ambitious.pei@gmail.com>
2016-03-23 20:44:48 +0800

    [Time Limit Exceeded]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.swift
+++ 313-Super-Ugly-Number/solution.swift
@@ -4,9 +4,7 @@ class Solution {
         var indexes = [Int](count: primes.count, repeatedValue: 0)
         
         while uglNumbers.count < n {
-            NSLog("Indexes: \(indexes)")
             let stepResults = indexes.enumerate().map({ uglNumbers[$1] * primes[$0] })
-            NSLog("Step result: \(stepResults)")
             let min = stepResults.sort(<).first!
             
             for (index, number) in stepResults.enumerate() {
@@ -16,7 +14,6 @@ class Solution {
             }
             
             uglNumbers.append(min)
-            NSLog("Result: \(uglNumbers)")
         }
         
         return uglNumbers.last!
