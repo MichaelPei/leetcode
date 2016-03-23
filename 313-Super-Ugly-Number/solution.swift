9c94f02d9bfaf85d71d2ba7363872c25dc161e68
Michael Pei <ambitious.pei@gmail.com>
2016-03-23 20:31:19 +0800

    [Runtime Error]committed by LeetCode Extension

---
+++ 313-Super-Ugly-Number/solution.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func nthSuperUglyNumber(n: Int, _ primes: [Int]) -> Int {
+        var uglNumbers = [1]
+        var indexes = [Int](count: 3, repeatedValue: 0)
+        
+        while uglNumbers.count < n {
+            let stepResults = indexes.enumerate().map({ uglNumbers[$1] * primes[$0] })
+            let min = stepResults.sort(>).first!
+            
+            for (index, number) in stepResults.enumerate() {
+                if number == min {
+                    indexes[index] += 1
+                }
+            }
+            
+            uglNumbers.append(min)
+        }
+        
+        return uglNumbers.last!
+    }
+}
