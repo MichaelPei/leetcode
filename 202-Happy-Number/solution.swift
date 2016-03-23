4f791bd39a8618486ddc2bb125ae932da8486d7f
Michael Pei <ambitious.pei@gmail.com>
2016-03-23 17:04:23 +0800

    [Compile Error]committed by LeetCode Extension

--- 202-Happy-Number/solution.swift
+++ 202-Happy-Number/solution.swift
@@ -3,11 +3,13 @@ class Solution {
         var number = n
         var newNumber = 0
         while number > 0 {
+            NSLog("Begin: number:: \(number), new: \(newNumber)")
             newNumber += Int(pow(Double(number % 10), 2))
             number /= 10
+            NSLog("End: number:: \(number), new: \(newNumber)")
         }
         
-        if newNumber == 0 || newNumber == 1 {
+        if newNumber < 10 && newNumber != 7{
             return newNumber == 1
         }
         else {
